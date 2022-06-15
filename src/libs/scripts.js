b19d51cf3d74e49d56f198a0b2c92e0bdee0fff4
flashgrinder <flashgrinder92@gmail.com>
2022-06-15 12:17:55 +0400

    make mobile menu

--- src/libs/scripts.js
+++ src/libs/scripts.js
@@ -1,7 +1,14 @@
+import gsap from 'gsap';
 import modalsForm from '../libs/modules/modals-form.js';
+import burgerMenu from '../libs/modules/burger-menu.js';
 
 document.addEventListener('DOMContentLoaded', function(e) {
+
+    gsap.config({
+        nullTargetWarn: false
+    });
     
     modalsForm.init();
+    burgerMenu.init();
 
 });
